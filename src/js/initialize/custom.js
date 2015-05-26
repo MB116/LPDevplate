e28ed07475be2752ff33dc5d86f0e53a36023696
KanaSahanov <kana_sahanov@mail.ru>
2015-05-26 11:22:58 +0600

    Title scss created, gitignore modified

--- src/js/initialize/custom.js
+++ src/js/initialize/custom.js
@@ -5,13 +5,6 @@
 
 $(document).ready(function() {
     //Analytics bounce rate trigger
-    $(window).on('scroll.custom', function () {
-        var windowScroll = $(this).scrollTop();
 
-        if(windowScroll >= 200){
-            ga('send', 'event', 'ОТКАЗ', 'СКРОЛЛ');
-            $(window).off('scroll.custom');
-        }
-    });
 
 });
